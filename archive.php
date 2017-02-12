13bd642d6bca2d33d4666581cb9d6647843cb2e2
metheno <e2333666@gmail.com>
2017-02-12 12:27:10 +0800

    Revert "Revert "Modify components""
    This reverts commit 0aba3ba76c17d768704b7960c87aa59e9367990c.

---
+++ archive.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * 让我们为梦想而努力吧！
+ *
+ * @package Kibou
+ * @author metheno
+ * @version 0.1
+ * @link http://www.metheno.net
+ */
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+$this->need('header.php'); ?>
+
+<div class="blog-main">
+
+  <center>
+    <h4 class="archive-title"><?php $this->archiveTitle(array(
+        'category'  =>  _t('分类 %s 下的文章'),
+        'search'    =>  _t('含 %s 的文章'),
+        'tag'       =>  _t('标签 %s 下的文章'),
+        'author'    =>  _t('%s 发布的文章')
+    ), '', ''); ?></h4>
+  </center>
+
+  <?php if ($this->have()): ?>
+
+    <?php while($this->next()): ?>
+      <article itemscope="" itemtype="http://schema.org/BlogPosting">
+        <div class="blog-post">
+          <h2 class="blog-post-title" itemprop="name headline"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
+          <p class="blog-post-meta">
+            <a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a>&nbsp;•
+            <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time>&nbsp;•
+            <?php $this->category(', '); ?>
+          </p>
+
+          <?php $this->content(); ?>
+
+          <center class="center-more"><a href="<?php $this->permalink() ?>" class="btn read-more"><?php _e('阅读更多'); ?></a></center>
+        </div>
+      </article><!-- /.blog-post -->
+    <?php endwhile; ?>
+
+  <?php else: ?>
+      <article>
+          <h2 class="blog-post-title"><?php _e('没有找到内容'); ?></h2>
+      </article>
+  <?php endif; ?>
+
+  <hr/>
+
+  <center>
+    <?php $this->pageNav('&laquo;', '&raquo;', 5, '...', array('wrapTag' => 'ul', 'wrapClass' => 'pagination', 'itemTag' => 'li', 'textTag' => 'span', 'currentClass' => 'active', 'prevClass' => 'prev', 'nextClass' => 'next')); ?>
+  </center>
+
+</div>
+
+<?php $this->need('footer.php'); ?>
