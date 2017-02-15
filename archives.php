b9557edae8a2a59f19ee52bf9b7a7b21ae0ee244
metheno <e2333666@gmail.com>
2017-02-16 02:04:58 +0800

    Update Tag Cloud

--- archives.php
+++ archives.php
@@ -11,19 +11,21 @@ $this->need('header.php'); ?>
 
 <article itemscope="" itemtype="http://schema.org/BlogPosting">
   <div class="blog-post">
-    <h2 class="blog-post-title" itemprop="name headline"><?php $this->title() ?></h2>
+    <h2 class="blog-post-title" itemprop="name headline"><?php _e('Tag Cloud'); ?></h2>
 
-    <div class="center-more">
-      <?php $this->widget('Widget_Metas_Tag_Cloud', 'sort=mid&ignoreZeroCount=1&desc=0&limit=30')->to($tags); ?>
+    <div class="center-padding">
+      <?php $this->widget('Widget_Metas_Tag_Cloud', 'sort=mid&ignoreZeroCount=1&desc=0&limit=100000')->to($tags); ?>
 
       <?php if($tags->have()): ?>
         <?php while ($tags->next()): ?>
-          <a href="<?php $tags->permalink(); ?>" rel="tag" class="tags" title="<?php $tags->count(); ?> 个话题"><?php $tags->name(); ?></a>
+          <a href="<?php $tags->permalink(); ?>" rel="tag" class="btn btn-sm btn-transparent" title="<?php $tags->count(); ?> 个话题"><?php $tags->name(); ?></a>
         <?php endwhile; ?>
         <?php else: ?>
           <p><?php _e('没有任何标签'); ?></p>
       <?php endif; ?>
     </div>
+
+    <?php $this->widget('Widget_Contents_Post_Recent', 'pageSize=10000')->to($archives);
 
     <?php $this->content(); ?>
     <?php $this->need('comments.php'); ?>
@@ -32,3 +34,12 @@ $this->need('header.php'); ?>
 </article><!-- /.blog-post -->
 
 <?php $this->need('footer.php'); ?>
+
+
+<?php
+$money = 0;
+$sleep = false;
+while ($sleep == true):
+  $money++;
+endwhile;
+?>
