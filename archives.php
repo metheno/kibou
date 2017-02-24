beb9513b6ba5b2c053f73dc7592f475112197e41
metheno <e2333666@gmail.com>
2017-02-24 21:40:52 +0800

    Optimize archives.php | Update to 0.1.6

--- archives.php
+++ archives.php
@@ -13,17 +13,28 @@ $this->need('header.php'); ?>
   <div class="blog-post">
     <h2 class="blog-post-title" itemprop="name headline"><?php _e('Tag Cloud'); ?></h2>
 
-    <div class="center-padding">
+    <div style="padding-top: 20px;">
       <?php $this->widget('Widget_Metas_Tag_Cloud', 'sort=mid&ignoreZeroCount=1&desc=0&limit=1000')->to($tags); ?>
 
-      <?php if($tags->have()): ?>
+      <?php if($tags->have()):?>
         <?php while ($tags->next()): ?>
-          <a href="<?php $tags->permalink(); ?>" rel="tag" class="btn btn-sm btn-transparent" title="<?php $tags->count(); ?> 个话题"><?php $tags->name(); ?></a>
+          <a href="<?php $tags->permalink(); ?>" rel="tag" class="archives-tags" title="<?php $tags->count(); ?> 个话题"><?php $tags->name(); ?></a>
         <?php endwhile; ?>
         <?php else: ?>
           <p><?php _e('没有任何标签'); ?></p>
       <?php endif; ?>
     </div>
+
+    <hr/>
+
+    <ol class="archives-loop-ol">
+      <?php
+        $this->widget('Widget_Contents_Post_Recent', 'pageSize=10')->to($archives);
+        while($archives->next()):
+      ?>
+      <a class="archives-loop-li" href="<?php $archives->permalink() ?>"><li itemprop="name headline"><span class="text-muted"><?php $archives->date('Y-m-d'); ?></span>  <?php $archives->title() ?></li></a>
+      <?php endwhile; ?>
+    </ol>
 
     <?php $this->content(); ?>
     <?php $this->need('comments.php'); ?>
