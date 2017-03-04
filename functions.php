eea53c73422295ff2fdc553b4f8cace6370c532e
metheno <e2333666@gmail.com>
2017-03-04 11:10:46 +0800

    Minor changes.

--- functions.php
+++ functions.php
@@ -15,21 +15,21 @@ function themeConfig($form) {
   $enableTransparent = new Typecho_Widget_Helper_Form_Element_Radio('enableTransparent',
     array('1' => _t('开启'),
     '0' => _t('关闭')),
-    '0', _t('Safari 毛玻璃特效 (Beta)'), _t('默认为关闭'));
+    '0', _t('毛玻璃特效 (Beta)'), _t('默认为关闭。<br/>现已支持 Chrome 和 Firefox 浏览器。'));
   $form->addInput($enableTransparent);
 
   $enableTransparentBg = new Typecho_Widget_Helper_Form_Element_Text('enableTransparentBg', NULL, NULL,
-  _t('毛玻璃特效 背景图'), _t('在这里填入一个图片的 URL 地址。<br/>该设置仅在 <b>Safari 毛玻璃特效</b> <b style="color:red;">开启</b> 时生效，且仅在 Safari 可见。'));
+  _t('毛玻璃特效 背景图'), _t('在这里填入一个图片的 URL 地址。<br/>该设置仅在 <b>毛玻璃特效</b> <b style="color:red;">开启</b> 时生效。'));
   $form->addInput($enableTransparentBg);
 
   $enableTransparentBgPosition = new Typecho_Widget_Helper_Form_Element_Text('enableTransparentBgPosition', NULL, NULL,
-  _t('毛玻璃特效 背景图位置'), _t('为 <code>body</code> 添加一个 <code>background-position</code>。例如：<code>right</code>。<br/>该设置仅在 <b>Safari 毛玻璃特效</b> <b style="color:red;">开启</b> 时生效，且仅在 Safari 可见。'));
+  _t('毛玻璃特效 背景图位置'), _t('为 <code>body</code> 添加一个 <code>background-position</code>。例如：<code>right</code>。<br/>该设置仅在 <b>毛玻璃特效</b> <b style="color:red;">开启</b> 时生效。'));
   $form->addInput($enableTransparentBgPosition);
 
   $enableSerifFont = new Typecho_Widget_Helper_Form_Element_Radio('enableSerifFont',
     array('1' => _t('开启'),
     '0' => _t('关闭')),
-    '0', _t('正文使用衬线字体'), _t('关闭状况下<b>正文</b>字体使用 Roboto、苹方、微软雅黑。开启后使用 Roboto Slab。<br/>该设置对各个标题不生效。'));
+    '0', _t('正文使用衬线字体'), _t('关闭状况下<b>正文</b>字体使用 Roboto、苹方、微软雅黑。开启后使用 Roboto Slab。'));
   $form->addInput($enableSerifFont);
 
   $enableMathJax = new Typecho_Widget_Helper_Form_Element_Radio('enableMathJax',
