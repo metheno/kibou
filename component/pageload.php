6eb39e30c424f7b63d839bb906b7792673549a41
metheno <e2333666@gmail.com>
2017-03-25 00:46:03 +0800

    Update to 0.2

---
+++ component/pageload.php
@@ -0,0 +1,95 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+<?php if (($this->options->enableTransparent == 1) && (UACheck::isSafari() == true)): ?>
+  <?php if ((UACheck::isMobile() == false)): ?>
+  <link href="<?php $this->options->themeUrl('css/transparent.css'); ?>" rel="stylesheet">
+  <style>
+    @supports (-webkit-backdrop-filter: blur(20px)) or (backdrop-filter: blur(20px)) {
+      body {
+        background-image: url('<?php if ($this->options->enableTransparentBg): $this->options->enableTransparentBg(); endif; ?>');
+        background-position: <?php if ($this->options->enableTransparentBgPosition): $this->options->enableTransparentBgPosition(); endif; ?>;
+      }
+      <?php if ($this->options->frostedGlassRGBA): ?>
+      .blog-masthead, .blog-main {
+        background-color: <?php $this->options->frostedGlassRGBA(); ?>;
+      }
+      <?php endif; ?>
+      <?php if ($this->options->frostedGlassBlurAmount): ?>
+      .blog-masthead, .blog-main, .blog-footer {
+        -webkit-backdrop-filter: blur(<?php $this->options->frostedGlassBlurAmount(); ?>px);
+        backdrop-filter: blur(<?php $this->options->frostedGlassBlurAmount(); ?>);
+      }
+      <?php endif; ?>
+    }
+  </style>
+  <?php endif; ?>
+
+<?php endif; ?>
+
+<?php if (($this->options->themeColor) && ((UACheck::isSafari() == false) || (UACheck::isMobile() == true))): ?>
+  <style>
+    a, a:hover, a:active,
+    .pagination > .active > a,.pagination > .active > a:focus,.pagination > .active > a:hover,
+    .pagination > .active > span,.pagination > .active > span:focus,.pagination > .active > span:hover,
+    .pagination > li > a, .pagination > li > a:focus, .pagination > li > a:hover, .pagination > li > span,
+    .blog-title a, h2.blog-post-title a {
+      color: <?php $this->options->themeColor(); ?>;
+      border-color: <?php $this->options->themeColor(); ?>;
+    }
+    .blog-masthead {
+      background-color: <?php $this->options->themeColor(); ?>;
+    }
+  </style>
+<?php endif; ?>
+
+<?php if ($this->options->enableSerifFont == 1): ?>
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700" rel="stylesheet">
+  <style>
+    body {
+      font-family: 'Roboto Slab', serif;
+    }
+  </style>
+<?php else: ?>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100italic,300italic,400italic,600italic,700italic,100,300,400,600,700" rel="stylesheet">
+  <style>
+    body {
+      font-family: 'Roboto', 'Pingfang SC', 'Microsoft Yahei', sans-serif;
+    }
+  </style>
+<?php endif; ?>
+
+<?php if ((PluginCheck::aPlayerExists() == true) && (UACheck::isSafari() == true)): ?>
+  <style>
+    .aplayer-info {
+      color: rgba(80, 80, 80, 1);
+      background-color: rgba(255, 255, 255, 1);
+    }
+
+    .aplayer-list {
+      color: rgba(80, 80, 80, 1);
+      background-color: rgba(255, 255, 255, 1);
+    }
+  </style>
+<?php endif; ?>
+
+<?php if (PluginCheck::dPlayerExists() == true): ?>
+  <style>
+    .dplayer-video {
+      border-radius: 0;
+      margin: 0;
+    }
+    .dplayer-menu-label a:hover {
+      text-decoration: none;
+    }
+  <?php if (UACheck::isSafari() == true): ?>
+    .dplayer .dplayer-menu {
+      box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.5);
+      -webkit-backdrop-filter: blur(4px);
+      backdrop-filter: blur(4px);
+    }
+    .dplayer-menu-label a, .dplayer-menu-label a:hover {
+      text-decoration: none;
+    }
+  <?php endif; ?>
+  </style>
+<?php endif; ?>
---
+++ lib/UACheck.php
@@ -0,0 +1,58 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+/**
+ * UACheck.php
+ * Author     : metheno
+ * Date       : 2017/03/12
+ * Version    :
+ * Description:
+ */
+class UACheck {
+
+  private static $_userAgent;
+
+  private static function getUserAgent() {
+    self::$_userAgent = $_SERVER['HTTP_USER_AGENT'];
+    return self::$_userAgent;
+
+  }
+
+  public static function isSafari() {
+    if (strpos(self::getUserAgent(), 'Safari') && !strpos(self::getUserAgent(), 'Chrome')) {
+      return true;
+    }
+  }
+
+  public static function isMobile() {
+    if (stripos(self::getUserAgent(), 'Mobile')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static function isMacOsX() {
+    if (stripos(self::getUserAgent(), 'Macintosh; Intel Mac OS X')) {
+      return true;
+    }
+  }
+
+  public static function isWindows() {
+    if (stripos(self::getUserAgent(), 'Windows NT')) {
+      return true;
+    }
+  }
+
+  public static function isMSIE() {
+    if (stripos(self::getUserAgent(), 'MSIE')) {
+      return true;
+    }
+  }
+
+  public static function isEdge() {
+    if (stripos(self::getUserAgent(), 'Edge')) {
+      return true;
+    }
+  }
+}
