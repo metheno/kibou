86871cd58ed0a3aeeb4c4dddeec663163f02ae4d
metheno <e2333666@gmail.com>
2017-03-26 10:19:28 +0800

    Fix merge errors.

--- index.php
+++ index.php
@@ -4,11 +4,7 @@
  *
  * @package Kibou
  * @author metheno
-<<<<<<< HEAD
  * @version 0.2.1
-=======
- * @version 0.2
->>>>>>> origin/master
  * @link http://www.metheno.net
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
